e6a66060954e9e9e24ad5ef54e15a72c5f45ad52
The-Cubans-Corner <147349769+TheCubansCorner@users.noreply.github.com>
2024-04-04 12:26:37 -0400

    Minor Adjustments
    Added more buttons for future functionality
    
    Adjusted placement of some widgets in code

--- main.py
+++ main.py
@@ -28,34 +28,53 @@ class MainWindow(QMainWindow):
         self.setFixedSize(600, 800)
         self.initWidget()
         self.initLayout()
-        self.filterWidgetLayout()
         self.initConfigWidgets()
         self.initConfigConnections()
         self.initStyleSheets()
         self.widgetToolTips()
-        self.setCentralWidget(self.mainContainer)
         self.initFilterMenu()
+        self.setCentralWidget(self.mainContainer)
 
         self.labelWidth: int = self.imageLabel.width()
         self.labelHeight: int = self.imageLabel.height()
 
     def initWidget(self) -> None:                       # -- Initiates main window widgets
-        self.mainContainer: QWidget = QWidget()                             # - QWidgets
+        self.mainContainer: QWidget = QWidget()                                                       # - QWidgets
         self.colapsedMenu: QWidget = QWidget()
         self.expandedMenu: QWidget = QWidget()
         self.mainApplication: QWidget = QWidget()
         
         # Colapsed Menu Buttons
-        self.colapedMenuBtn: QWidget = QPushButton()                        # - QPushButtons
+        self.colapsedMenuBtn: QWidget = QPushButton()                                                 # - QPushButtons Colapseed
         self.colapsedLoadImageBtn: QWidget = QPushButton()
+        self.colapsedBrightnessBtn: QWidget = QPushButton()
+        self.colapsedContrastBtn: QWidget = QPushButton()
+        self.colapsedFiltersBtn: QWidget = QPushButton()  
+        self.colapsedRotationBtn: QWidget = QPushButton()
+        self.colapsedSaveBtn: QWidget = QPushButton()
         
         # Expanded menu Buttons
-        self.expandedMenuBtn: QWidget = QPushButton("Colapse")
+        self.expandedMenuBtn: QWidget = QPushButton("Colapse")                                        # - QPushButton Expanded
         self.expandedLoadImageBtn: QWidget = QPushButton("Load Image")
+        self.expandedFiltersBtn: QWidget = QPushButton("        Filters")
+        self.expandedBlackWhiteBtn: QWidget = QPushButton()
+        self.expandedSmoothBtn: QWidget = QPushButton() 
+        self.expandedSharpenBtn: QWidget = QPushButton()
+        self.expandedEmbossBtn: QWidget = QPushButton()
+        self.expandedRotateRightBtn: QWidget = QPushButton()
+        self.expandedRotateLeftBtn: QWidget = QPushButton()
+        self.expandedSaveBtn: QWidget = QPushButton("Save Image")
+        self.expandedEdgeBtn: QWidget = QPushButton()
+        self.expandedBlurBtn: QWidget = QPushButton()
+        self.expandedDetailBtn: QWidget = QPushButton()
+        self.expandedContourBtn: QWidget = QPushButton()
         
-        #self.expandedBlackWhiteSlide: QWidget = QSlider(Qt.Orientation.Horizontal)                   # - QSliders
+        self.expandedBrightnessSlide: QWidget = QSlider(Qt.Orientation.Horizontal)                   # - QSliders
+        self.expandedContrastSlide: QWidget = QSlider(Qt.Orientation.Horizontal)
 
-        self.imageLabel: QWidget = QLabel()                                 # - QLabels
+        self.imageLabel: QWidget = QLabel()                                                           # - QLabels
+        self.expandedBrightnessLabel: QWidget = QLabel("Brightness")
+        self.expandedContrastLabel: QWidget = QLabel("Contrast")
 
     def initLayout(self) -> None:                       # -- Applies widgets to layout/sets main layout
         self.mainContainer.layout = QHBoxLayout()       # - Main Layouts
@@ -63,13 +82,54 @@ class MainWindow(QMainWindow):
         self.expandedMenu.layout = QGridLayout()
         self.mainApplication.layout = QVBoxLayout()
 
-        self.colapsedMenu.layout.addWidget(self.colapedMenuBtn, 0, 0, Qt.AlignmentFlag.AlignLeft)
-        self.colapsedMenu.layout.addWidget(self.colapsedLoadImageBtn, 1, 0, Qt.AlignmentFlag.AlignLeft)   
+        filtersLayout = QVBoxLayout()                   # - Filter Layouts
+        headerlayout = QHBoxLayout()
+        rowOneLayout = QHBoxLayout()
+        rowTwoLayout = QHBoxLayout()
 
-        self.expandedMenu.layout.addWidget(self.expandedMenuBtn, 0, 0, Qt.AlignmentFlag.AlignLeft)
+        brightnessLayout = QHBoxLayout()
+        contrastLayout = QHBoxLayout()
+        rotationLayout = QHBoxLayout()
+
+        headerlayout.addWidget(self.expandedFiltersBtn)                                                         # - FIlter Menu
+        rowOneLayout.addWidget(self.expandedBlackWhiteBtn)
+        rowOneLayout.addWidget(self.expandedSmoothBtn)
+        rowOneLayout.addWidget(self.expandedSharpenBtn)
+        rowOneLayout.addWidget(self.expandedEmbossBtn)
+        rowTwoLayout.addWidget(self.expandedEdgeBtn)
+        rowTwoLayout.addWidget(self.expandedBlurBtn)
+        rowTwoLayout.addWidget(self.expandedDetailBtn)
+        rowTwoLayout.addWidget(self.expandedContourBtn)
+        filtersLayout.addLayout(headerlayout)
+        filtersLayout.addLayout(rowOneLayout)
+        filtersLayout.addLayout(rowTwoLayout)
+
+        brightnessLayout.addWidget(self.expandedBrightnessLabel)                                                # - Brightness Menu
+        brightnessLayout.addWidget(self.expandedBrightnessSlide)
+
+        contrastLayout.addWidget(self.expandedContrastLabel)                                                    # - Contrast Menu
+        contrastLayout.addWidget(self.expandedContrastSlide)
+
+        rotationLayout.addWidget(self.expandedRotateLeftBtn)                                                    # - Rotation Menu
+        rotationLayout.addWidget(self.expandedRotateRightBtn)
+
+        self.colapsedMenu.layout.addWidget(self.colapsedMenuBtn, 0, 0, Qt.AlignmentFlag.AlignLeft)              # - Colapsed Tool Menu
+        self.colapsedMenu.layout.addWidget(self.colapsedLoadImageBtn, 1, 0, Qt.AlignmentFlag.AlignLeft)
+        self.colapsedMenu.layout.addWidget(self.colapsedSaveBtn, 2, 0, Qt.AlignmentFlag.AlignLeft)
+        self.colapsedMenu.layout.addWidget(self.colapsedFiltersBtn, 3, 0, Qt.AlignmentFlag.AlignLeft)  
+        self.colapsedMenu.layout.addWidget(self.colapsedBrightnessBtn, 4, 0, Qt.AlignmentFlag.AlignLeft)
+        self.colapsedMenu.layout.addWidget(self.colapsedContrastBtn, 5, 0, Qt.AlignmentFlag.AlignLeading)
+        self.colapsedMenu.layout.addWidget(self.colapsedRotationBtn, 6, 0, Qt.AlignmentFlag.AlignLeft)
+
+        self.expandedMenu.layout.addWidget(self.expandedMenuBtn, 0, 0, Qt.AlignmentFlag.AlignLeft)              # - Expanded Tool Menu
         self.expandedMenu.layout.addWidget(self.expandedLoadImageBtn, 1, 0, Qt.AlignmentFlag.AlignLeft) 
+        self.expandedMenu.layout.addWidget(self.expandedSaveBtn, 2, 0, Qt.AlignmentFlag.AlignLeft)
+        self.expandedMenu.layout.addLayout(filtersLayout, 3, 0, Qt.AlignmentFlag.AlignLeft)
+        self.expandedMenu.layout.addLayout(brightnessLayout, 4, 0, Qt.AlignmentFlag.AlignLeft)
+        self.expandedMenu.layout.addLayout(contrastLayout, 5, 0, Qt.AlignmentFlag.AlignLeft)
+        self.expandedMenu.layout.addLayout(rotationLayout, 6, 0, Qt.AlignmentFlag.AlignLeft)
 
-        self.mainApplication.layout.addWidget(self.imageLabel, Qt.AlignmentFlag.AlignCenter)
+        self.mainApplication.layout.addWidget(self.imageLabel, Qt.AlignmentFlag.AlignCenter)                    # - Main Layout
 
         self.colapsedMenu.setLayout(self.colapsedMenu.layout)
         self.expandedMenu.setLayout(self.expandedMenu.layout)
@@ -83,31 +143,54 @@ class MainWindow(QMainWindow):
 
     def initConfigWidgets(self) -> None:                # -- Configured widgets (size, width, etc)
         self.expandedMenu.hide()
-        self.mainContainer.layout.setContentsMargins(0, 0, 0, 0)
 
-        self.imageLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)                                  # - Alignments
+        self.imageLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)                                   # - Alignments
 
-        pixmap = QPixmap(os.path.join("icons", "blank.jpg"))                                        # - Setting Pixmaps
+        pixmap = QPixmap(os.path.join("icons", "blank.jpg"))                                         # - Setting Initial Pixmap
         self.imageLabel.setPixmap(pixmap)
 
-        self.colapedMenuBtn.setIcon(QIcon(os.path.join("icons", "colapsed-menu.png")))              # - Setting Icons
-        self.colapsedLoadImageBtn.setIcon(QIcon(os.path.join("icons", "save-icon.png")))
-        self.colapsedFiltersBtn.setIcon(QIcon(os.path.join("icons", "filters-icon.png")))
+        self.colapsedMenuBtn.setIcon(QIcon(os.path.join("icons", "colapsed-menu.png")))              # - Setting Icons
+        self.colapsedLoadImageBtn.setIcon(QIcon(os.path.join("icons", "open-img-icon.png")))
+        self.colapsedFiltersBtn.setIcon(QIcon(os.path.join("icons", "filters-icon.png")))     
+        self.colapsedBrightnessBtn.setIcon(QIcon(os.path.join("icons", "brightness-icon.png")))
+        self.colapsedContrastBtn.setIcon(QIcon(os.path.join("icons", "contrast-icon.png")))
+        self.colapsedRotationBtn.setIcon(QIcon(os.path.join("icons", "rotate-right.png")))
+        self.colapsedSaveBtn.setIcon(QIcon(os.path.join("icons", "save-icon.png")))
+        
         self.expandedMenuBtn.setIcon(QIcon(os.path.join("icons", "expanded-menu.png")))
-        self.expandedLoadImageBtn.setIcon(QIcon(os.path.join("icons", "save-icon.png")))
+        self.expandedLoadImageBtn.setIcon(QIcon(os.path.join("icons", "open-img-icon.png")))
         self.expandedBlackWhiteBtn.setIcon(QIcon(os.path.join("icons", "black-white-icon.png")))
         self.expandedFiltersBtn.setIcon(QIcon(os.path.join("icons", "filters-icon.png")))
         self.expandedSmoothBtn.setIcon(QIcon(os.path.join("icons", "smooth-filter.png")))
         self.expandedSharpenBtn.setIcon(QIcon(os.path.join("icons", "sharpen-icon.png")))
         self.expandedEmbossBtn.setIcon(QIcon(os.path.join("icons", "emboss-icon.png")))
+        self.expandedSaveBtn.setIcon(QIcon(os.path.join("icons", "save-icon.png")))
+        self.expandedRotateRightBtn.setIcon(QIcon(os.path.join("icons", "rotate-right.png")))
+        self.expandedRotateLeftBtn.setIcon(QIcon(os.path.join("icons", "rotate-left.png")))
+        self.expandedEdgeBtn.setIcon(QIcon(os.path.join("icons", "edge-icon.png")))
+        self.expandedBlurBtn.setIcon(QIcon(os.path.join("icons", "blur-icon.png")))
+        self.expandedDetailBtn.setIcon(QIcon(os.path.join("icons", "detail-icon.png")))
+        self.expandedContourBtn.setIcon(QIcon(os.path.join("icons", "contour-icon.png")))
 
-        self.mainApplication.setMaximumHeight(self.height())                                        # - Max Width/Height
+
+        self.mainContainer.layout.setContentsMargins(0, 0, 0, 0)                                    # - Content Margins
+        self.colapsedMenuBtn.setContentsMargins(0,0,0,0)
+        self.colapsedFiltersBtn.setContentsMargins(0,0,0,0)
+        self.colapsedBrightnessBtn.setContentsMargins(0,0,0,0)
+        self.colapsedMenu.setContentsMargins(0, 0, 0, 0)
+
+        self.mainApplication.setMaximumHeight(self.height())                                        # - Max Size
         self.colapsedMenu.setMaximumWidth(40)
         self.expandedMenu.setMaximumWidth(150)
         self.expandedMenu.setMinimumWidth(150)
+        self.expandedBlackWhiteBtn.setMaximumSize(25,25)                                        
+        self.expandedSmoothBtn.setMaximumSize(25,25)
+        self.expandedBlurBtn.setMaximumSize(25,25)
+        self.expandedDetailBtn.setMaximumSize(25,25)
+        self.expandedEdgeBtn.setMaximumSize(25,25)
 
     def initConfigConnections(self) -> None:            # -- Configures connections between buttons and functions
-        self.colapedMenuBtn.clicked.connect(self.expandSideMenu)
+        self.colapsedMenuBtn.clicked.connect(self.expandSideMenu)
         self.expandedMenuBtn.clicked.connect(self.colapseSideMenu)
         self.colapsedLoadImageBtn.clicked.connect(self.openImageDialog)
         self.expandedLoadImageBtn.clicked.connect(self.openImageDialog)
@@ -121,45 +204,30 @@ class MainWindow(QMainWindow):
         self.setStyleSheet("background-color: black;")
 
     def widgetToolTips(self) -> None:                   # -- Sets up widget hover tool tips
-        self.colapedMenuBtn.setToolTip("Expand Menu")
-        self.expandedMenuBtn.setToolTip("Colapse Menu")
+        self.colapsedMenuBtn.setToolTip("Expand Menu")
+        self.colapsedBrightnessBtn.setToolTip("Brightness")
+        self.colapsedContrastBtn.setToolTip("Contrast")
         self.colapsedLoadImageBtn.setToolTip("Load Image")
-        self.expandedLoadImageBtn.setToolTip("Load Image")  
         self.colapsedFiltersBtn.setToolTip("Filters")
+        self.colapsedRotationBtn.setToolTip("Rotation")
+        self.colapsedSaveBtn.setToolTip("Save Image")
+
+        self.expandedMenuBtn.setToolTip("Colapse Menu")
+        self.expandedLoadImageBtn.setToolTip("Load Image") 
         self.expandedBlackWhiteBtn.setToolTip("Black/White")
         self.expandedSmoothBtn.setToolTip("Smooth")
         self.expandedSharpenBtn.setToolTip("Sharpen")
-        self.expandedEmbossBtn.setToolTip("Emboss")
-
-    def filterWidgetLayout(self) -> None:               # -- Applies Layouts for the filterWidgets
-        filtersLayout = QVBoxLayout()
-        headerlayout = QHBoxLayout()
-        rowOneLayout = QHBoxLayout()
-        rowTwoLayout = QHBoxLayout()
-
-        self.colapsedFiltersBtn: QWidget = QPushButton()
-        self.expandedFiltersBtn: QWidget = QPushButton("        Filters")
-        self.expandedBlackWhiteBtn: QWidget = QPushButton()
-        self.expandedSmoothBtn: QWidget = QPushButton() 
-        self.expandedSharpenBtn: QWidget = QPushButton()
-        self.expandedEmbossBtn: QWidget = QPushButton()
-
-        headerlayout.addWidget(self.expandedFiltersBtn)
-        rowOneLayout.addWidget(self.expandedBlackWhiteBtn)
-        rowOneLayout.addWidget(self.expandedSmoothBtn)
-        rowOneLayout.addWidget(self.expandedSharpenBtn)
-        rowOneLayout.addWidget(self.expandedEmbossBtn)
-
-        
-
-        filtersLayout.addLayout(headerlayout)
-        filtersLayout.addLayout(rowOneLayout)
-
-        self.colapsedMenu.layout.addWidget(self.colapsedFiltersBtn, 2, 0, Qt.AlignmentFlag.AlignLeft)
-        self.expandedMenu.layout.addLayout(filtersLayout, 2, 0, Qt.AlignmentFlag.AlignLeft)
-
-        self.expandedBlackWhiteBtn.setMaximumSize(25,25)
-        self.expandedSmoothBtn.setMaximumSize(25,25)
+        self.expandedEmbossBtn.setToolTip("Emboss")    
+        self.expandedRotateLeftBtn.setToolTip("Rotate Left")
+        self.expandedRotateRightBtn.setToolTip("Rotate Right") 
+        self.expandedSaveBtn.setToolTip("Save Image")
+        self.expandedFiltersBtn.setToolTip("Filters")
+        self.expandedBrightnessLabel.setToolTip("Brightness")
+        self.expandedContrastLabel.setToolTip("Contrast")
+        self.expandedEdgeBtn.setToolTip("Edge Enhance")
+        self.expandedBlurBtn.setToolTip("Blur")
+        self.expandedDetailBtn.setToolTip("Detail Image")
+        self.expandedContourBtn.setToolTip("Contour")
 
     def expandSideMenu(self) -> None:                   # -- Expands the side menu
         self.colapsedMenu.hide()
@@ -215,12 +283,14 @@ class MainWindow(QMainWindow):
         self.filterMenu.addAction("Black/White")
         self.filterMenu.addAction("two")
         self.filterMenu.addAction("three")
+        
         self.filterMenu.hide()  
 
     def openMenu(self, type):                           # -- Opens Filter Context Menu
         if type == "filter":
             self.filterMenu.show()
-            self.filterMenu.move(700, 691)  
+            xy = self.colapsedFiltersBtn.mapToGlobal(self.colapsedFiltersBtn.pos())
+            self.filterMenu.move(xy)
 
     def mousePressEvent(self, event):
         x = event.position().x()
--- test.py
+++ test.py
@@ -1,26 +1,26 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QButtonGroup, QPushButton, QVBoxLayout, QWidget, QGroupBox
 from PyQt6.QtCore import Qt, QPoint
 
-class MouseTrackingApp(QMainWindow):
+class MouseTrackingApp(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Mouse Tracking Example")
-        self.setGeometry(100, 100, 400, 400)
+        group = QGroupBox()
+        x = QPushButton()
+        y = QPushButton()
+        z = QPushButton()
+        a = QLabel("BUttons")
 
-        self.mouse_coordinates_label = QLabel(self)
-        self.mouse_coordinates_label.setGeometry(10, 10, 150, 30)
-        self.mouse_coordinates_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        group.addAction(x)
+        group.addAction(y)
+        group.addAction(z)
 
-        # Track mouse movements in the main window
-        self.setMouseTracking(True)
-
-    def mousePressEvent(self, event):
-        x = event.position().x()
-        y = event.position().y()
-        print(x, y)
-        self.mouse_coordinates_label.setText(f"Mouse Coordinates: ({x}, {y})")
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(a)
+        self.layout.addWidget()
+        self.setLayout(self.layout)
+        
 
 
 def main():
